8c0f1cc38bef1c3e505e143b730f8d7da03f2623
Esaias Tilly <esaias@tillys.se>
2022-05-01 13:38:23 +0200

    Authenticate with Docker Hub on start

--- app.py
+++ app.py
@@ -3,7 +3,15 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-print(login(
-  os.getenv('DOCKER_USERNAME'),
-  os.getenv('DOCKER_PASSWORD')
-))
+def authenticate():
+  # Authenticate with Docker Hub
+  success = login(os.getenv('DOCKER_USERNAME'), os.getenv('DOCKER_PASSWORD'))
+
+  # Check if authentication was successful
+  if success: print('Authentication successful')
+  else: print('Authentication failed')
+
+  return success
+
+# Start by authenticating with Docker Hub
+if authenticate() == False: exit()
