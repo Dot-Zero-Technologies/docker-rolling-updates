9d3853757170bc704df4b9e2202224d0dbfdc102
Esaias Tilly <esaias@tillys.se>
2022-05-01 18:37:20 +0200

    Run application in loop

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import time
 from authentication import login
 import os
 from dotenv import load_dotenv
@@ -18,55 +19,61 @@ def authenticate():
 # Start by authenticating with Docker Hub
 if authenticate() == False: exit()
 
-# Get all containers and their repositories
-CONTAINERS = getContainers()
-REPOSITORIES = getRepositories(CONTAINERS)
+# Start application loop
+while True:
+  # Get all containers and their repositories
+  CONTAINERS = getContainers()
+  REPOSITORIES = getRepositories(CONTAINERS)
 
-# Go through each repository and get the latest image
-REPO_NAMES = [*REPOSITORIES.keys()]
-REPO_IMAGES = {}
-for repo in REPO_NAMES:
-  # Get the latest image for each tag
-  REPO_IMAGES[repo] = getRepositoryImagesByTag(repo)
-  print('Found ' + str(len(REPO_IMAGES[repo])) + ' images for ' + repo)
+  # Go through each repository and get the latest image
+  REPO_NAMES = [*REPOSITORIES.keys()]
+  REPO_IMAGES = {}
+  for repo in REPO_NAMES:
+    # Get the latest image for each tag
+    REPO_IMAGES[repo] = getRepositoryImagesByTag(repo)
+    print('Found ' + str(len(REPO_IMAGES[repo])) + ' images for ' + repo)
 
-# Check all container images and see if they match the latest image
-for container in CONTAINERS:
-  # Only check running containers
-  if container['inspect']['State']['Running'] != True: continue
+  # Check all container images and see if they match the latest image
+  for container in CONTAINERS:
+    # Only check running containers
+    if container['inspect']['State']['Running'] != True: continue
 
-  # Get the container image repository
-  containerImageRepo = container['inspect']['Config']['Image']
+    # Get the container image repository
+    containerImageRepo = container['inspect']['Config']['Image']
 
-  # Get the image digest
-  CONTAINER_IMAGE_DIGEST = getImageDigest(container['inspect']['Image'])
+    # Get the image digest
+    CONTAINER_IMAGE_DIGEST = getImageDigest(container['inspect']['Image'])
+    
+    # Get the namespace, repository, and tag
+    containerImageRepo = containerImageRepo.split('/')
+    CONTAINER_NAMESPACE = containerImageRepo[0]
+    CONTAINER_REPOSITORY = containerImageRepo[1].split(':')[0]
+    CONTAINER_TAG = containerImageRepo[1].split(':')[1]
+
+    # Get the newest image for the container tag
+    REPOSITORY_IMAGE_DIGEST = REPO_IMAGES[CONTAINER_NAMESPACE + '/' + CONTAINER_REPOSITORY][CONTAINER_TAG]
+
+    # Check if the container image matches the latest image
+    IS_UP_TO_DATE = CONTAINER_IMAGE_DIGEST == REPOSITORY_IMAGE_DIGEST
+
+    # Check if the container requires updating
+    if IS_UP_TO_DATE == False:
+      print(container['names'] + ' is out of date!')
+
+      # Pull the latest image
+      if pullImage(CONTAINER_NAMESPACE + '/' + CONTAINER_REPOSITORY + ':' + CONTAINER_TAG) != True:
+        print('Failed to pull image for ' + container['names'])
+        continue
+
+      # Restart the container
+      print('Restarting ' + container['names'])
+      if stopContainer(container['containerId']) != True:
+        print('Failed to stop ' + container['names'])
+        continue
+      if startContainer(container['containerId']) != True:
+        print('Failed to start ' + container['names'])
+        continue
+      print('Restarted ' + container['names'])
   
-  # Get the namespace, repository, and tag
-  containerImageRepo = containerImageRepo.split('/')
-  CONTAINER_NAMESPACE = containerImageRepo[0]
-  CONTAINER_REPOSITORY = containerImageRepo[1].split(':')[0]
-  CONTAINER_TAG = containerImageRepo[1].split(':')[1]
-
-  # Get the newest image for the container tag
-  REPOSITORY_IMAGE_DIGEST = REPO_IMAGES[CONTAINER_NAMESPACE + '/' + CONTAINER_REPOSITORY][CONTAINER_TAG]
-
-  # Check if the container image matches the latest image
-  IS_UP_TO_DATE = CONTAINER_IMAGE_DIGEST == REPOSITORY_IMAGE_DIGEST
-
-  # Check if the container requires updating
-  if IS_UP_TO_DATE == False:
-    print(container['names'] + ' is out of date!')
-
-    # Pull the latest image
-    if pullImage(CONTAINER_NAMESPACE + '/' + CONTAINER_REPOSITORY + ':' + CONTAINER_TAG) != True:
-      print('Failed to pull image for ' + container['names'])
-      continue
-
-    # Restart the container
-    print('Restarting ' + container['names'])
-    if stopContainer(container['containerId']) != True:
-      print('Failed to stop ' + container['names'])
-      continue
-    if startContainer(container['containerId']) != True:
-      print('Failed to start ' + container['names'])
-      continue
+  # Wait for a bit before checking again
+  time.sleep(int(os.getenv('SLEEP_TIME')))
