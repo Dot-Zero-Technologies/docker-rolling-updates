62b3341d34b81a66909f0138b5754135323d436a
Esaias Tilly <esaias@tillys.se>
2022-05-01 13:35:35 +0200

    Store authentication token in auth module

--- authentication.py
+++ authentication.py
@@ -1,8 +1,15 @@
 import requests
 
+# Docker Hub authentication token
+docker_hub_token = ''
+
 # Request a token from Docker Hub
 def login(username, password):
-  url = 'https://hub.docker.com/v2/users/login/'
-  data = {'username': username, 'password': password}
-  response = requests.post(url, data=data)
-  return response.json()['token']
+  try:
+    url = 'https://hub.docker.com/v2/users/login/'
+    data = {'username': username, 'password': password}
+    response = requests.post(url, data=data)
+    docker_hub_token = response.json()['token']
+    return True
+  except:
+    return False
