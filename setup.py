ba0b0d46446030eded7d8574cd96d8c4e6bb34ad
Esaias Tilly <esaias@tillys.se>
2022-05-02 11:39:47 +0200

    Created setup script

---
+++ setup.py
@@ -0,0 +1,29 @@
+import os
+
+# Check if the script is running as root
+if os.geteuid() != 0:
+  print('This script must be run as root')
+  exit()
+
+# Get current working directory
+WORKING_DIRECTORY = os.getcwd()
+
+# Write service file
+with open('/etc/systemd/system/docker-rolling-updates.service', 'w') as f:
+  f.write('[Unit]\n')
+  f.write('Description=Docker Rolling Updates\n')
+  f.write('\n')
+  f.write('[Service]\n')
+  f.write('WorkingDirectory=' + WORKING_DIRECTORY + '\n')
+  f.write('Type=simple\n')
+  f.write('ExecStart=/usr/bin/python3 ./app.py\n')
+  f.write('StandardInput=tty-force\n')
+  f.write('\n')
+  f.write('[Install]\n')
+  f.write('WantedBy=multi-user.target\n')
+print('Service file written')
+
+# Enable service
+os.system('systemctl enable docker-rolling-updates.service')
+
+print('To start the service, run: systemctl start docker-rolling-updates.service')
